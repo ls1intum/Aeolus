6ee6488cfead8b0ae3f5141be6757cf9d497be76
Andreas Resch <andreas@resch.io>
2023-08-17 19:45:53 +0200

    5 handle environment variables (#6)
    * add most important env variables
    
    * generate environment.py

--- cli/main.py
+++ cli/main.py
@@ -1,4 +1,6 @@
 # Press the green button in the gutter to run the script.
+from classes.windfile import Windfile
+
 if __name__ == "__main__":
-    Windfile
+    file: Windfile = Windfile()
     pass
