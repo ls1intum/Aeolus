a61893ec2d19dd2c8b6e1df983a23aeb4f37e3fe
Andreas Resch <andreas@resch.io>
2023-08-18 16:59:42 +0200

    add mypy and validate command

---
+++ cli/commands/validator.py
@@ -0,0 +1,154 @@
+import typing
+from typing import Optional
+
+import argparse
+import pydantic
+import yaml
+from io import TextIOWrapper
+
+from classes.generated.action import Actionfile
+from classes.generated.windfile import (
+    Windfile,
+    Action as WindAction,
+    ExternalAction,
+    InternalAction,
+    ExternalActionWithoutParameters,
+    ExternalActionWithParameters,
+)
+
+from typing import TypeVar
+
+
+def contains_external_actions(windfile: Windfile) -> bool:
+    """
+    Checks if the given windfile contains external actions.
+    :param windfile: input windfile
+    :return: True if the windfile contains external actions, False otherwise
+    """
+    for name in windfile.jobs:
+        action: WindAction = windfile.jobs[name]
+        if isinstance(action.root, ExternalAction):
+            return True
+    return False
+
+
+def get_external_actions(windfile: Windfile) -> typing.List[ExternalAction]:
+    """
+    Returns a list of all external actions in the given windfile.
+    :param windfile:
+    :return:
+    """
+    actions: typing.List[ExternalAction] = []
+    for name in windfile.jobs:
+        action: WindAction = windfile.jobs[name]
+        if isinstance(action.root, ExternalAction):
+            actions.append(action.root)
+    return actions
+
+
+def get_actions(
+        windfile: Windfile,
+) -> typing.List[
+    InternalAction | ExternalActionWithParameters | ExternalActionWithoutParameters
+    ]:
+    actions: typing.List[
+        InternalAction | ExternalActionWithParameters | ExternalActionWithoutParameters
+        ] = []
+    """
+    Returns a list of all actions in the given windfile.
+    :param windfile: Windfile to analyze
+    :return: List of actions
+    """
+    for name in windfile.jobs:
+        action: WindAction = windfile.jobs[name]
+        if isinstance(action.root, InternalAction):
+            actions.append(action.root)
+        elif isinstance(action.root, ExternalAction):
+            actions.append(action.root.root)
+    return actions
+
+
+def get_internal_actions(windfile: Windfile) -> typing.List[InternalAction]:
+    """
+    Returns a list of all internal actions in the given windfile.
+    :param windfile:
+    :return:
+    """
+    actions: typing.List[InternalAction] = []
+    for name in windfile.jobs:
+        action: WindAction = windfile.jobs[name]
+        if isinstance(action.root, InternalAction):
+            actions.append(action.root)
+    return actions
+
+
+class Validator:
+    T = TypeVar("T")
+
+    args: typing.Any
+
+    def __init__(self, args: typing.Any):
+        self.args = args
+
+    def add_arg_parser(parser: argparse.ArgumentParser):
+        parser.add_argument(
+            "--wind", "-w", help="Validate windfile.", action="store_true"
+        )
+        parser.add_argument(
+            "--action", "-a", help="Validate action.", action="store_true"
+        )
+        parser.add_argument(
+            "--input",
+            "-i",
+            help="Input file to read from",
+            default="windfile.yaml",
+            required=True,
+            type=open,
+        )
+
+    def read_file(self, filetype: T, path: TextIOWrapper) -> Optional[T]:
+        try:
+            typevalidator: pydantic.TypeAdapter = pydantic.TypeAdapter(filetype)
+            content: str = path.read()
+            file: Validator.T = typevalidator.validate_python(yaml.safe_load(content))
+            if file and self.args.verbose:
+                print(f"✅ {self.args.input.name} is valid")
+            return file
+        except pydantic.ValidationError as validation_error:
+            if self.args.verbose:
+                print(f"❌ {self.args.input.name} is invalid")
+            print(validation_error)
+            return None
+
+    def read_windfile(self, path: TextIOWrapper) -> Optional[Windfile]:
+        """
+        Validates the given file. If the file is valid, the windfile is returned.
+        :param path: path to input
+        :return: Windfile or None
+        """
+        return self.read_file(filetype=Windfile, path=path)
+
+    def read_actionfile(self, path: TextIOWrapper) -> Optional[Actionfile]:
+        """
+        Validates the given file. If the file is valid, the Actionfile is returned.
+        :param path: path to input
+        :return: Actionfile or None
+        """
+        return self.read_file(filetype=Actionfile, path=path)
+
+    def validate(self) -> Actionfile | Windfile | None:
+        """
+        Validates the given file. If the file is valid, the read object is returned.
+        If the file is invalid, None is returned.
+        :return: Actionfile or Windfile or None
+        """
+        if self.args.wind:
+            windfile: Optional[Windfile] = self.read_windfile(self.args.input)
+            if self.args.verbose:
+                if windfile and contains_external_actions(windfile):
+                    print("🌍This windfile contains external actions.")
+            return windfile
+        elif self.args.action:
+            action: Optional[Actionfile] = self.read_actionfile(self.args.input)
+            return action
+        return None
--- cli/main.py
+++ cli/main.py
@@ -1,6 +1,43 @@
-# Press the green button in the gutter to run the script.
-from classes.windfile import Windfile
+import argparse
+import sys
+from commands.validator import Validator
+
+
+def add_argparse() -> argparse.ArgumentParser:
+    arg_parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        prog="aeolus",
+        description="Aeolus is a tool to manage your CI jobs. It allows you to define your jobs in a simple "
+        "manner and execute them on multiple CI systems.",
+    )
+    subparsers = arg_parser.add_subparsers(dest="command")
+    arg_parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Increase output verbosity",
+        action="store_true",
+    )
+    arg_parser.add_argument(
+        "--version",
+        "-V",
+        help="Print version and exit",
+        action="version",
+        version="%(prog)s v0.0.1",
+    )
+    arg_parser.add_argument(
+        "--debug",
+        "-d",
+        help="Enable debug mode",
+        action="store_true",
+    )
+
+    validate_parser = subparsers.add_parser(name="validate")
+    Validator.add_arg_parser(validate_parser)
+    return arg_parser
+
 
 if __name__ == "__main__":
-    file: Windfile = Windfile()
-    pass
+    parser: argparse.ArgumentParser = add_argparse()
+    args = parser.parse_args(sys.argv[1:])
+    if args.command == "validate":
+        validator: Validator = Validator(args=args)
+        validator.validate()
