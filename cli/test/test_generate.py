0d34460ab9a59205b09a32b8947101841d51dbad
Andreas Resch <andreas@resch.io>
2023-12-19 20:06:21 +0100

    beautify

--- cli/test/test_generate.py
+++ cli/test/test_generate.py
@@ -131,9 +131,9 @@ class GenerateTests(unittest.TestCase):
             self.assertTrue(cli.check(content=result))
             # we change into the workdir, so we need to change back, and to be sure to always
             # be in the correct directory, we need to do this after every action
-            self.assertTrue(result.count("cd /aeolus") == 1)
+            self.assertTrue(result.count('cd "/aeolus"') == 1)
             self.assertIn("export AEOLUS_INITIAL_DIRECTORY=$(pwd)", result)
-            self.assertTrue(result.count("cd $AEOLUS_INITIAL_DIRECTORY") == len(windfile.actions) + 1)
+            self.assertTrue(result.count('cd "${AEOLUS_INITIAL_DIRECTORY}"') == len(windfile.actions) + 1)
 
     def test_generate_jenkinsfile_with_workdir(self) -> None:
         with TemporaryFileWithContent(WINDFILE_WITH_WORKDIR_ACTION) as file:
