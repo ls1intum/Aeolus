d66f829535c296b1b367e92ec6f66f50a7eedd8d
Andreas Resch <andreas@resch.io>
2024-01-07 18:22:06 +0100

    linting

--- cli/classes/bamboo_client.py
+++ cli/classes/bamboo_client.py
@@ -305,7 +305,9 @@ class BambooClient:
             timeout=30,
         )
         if response.status_code != 200:
-            raise Exception(f"Could not get plan {plan_key} from Bamboo: {response.text}")
+            raise Exception(
+                f"Could not get plan {plan_key} from Bamboo: {response.text}"
+                )  # pylint: disable=broad-exception-raised
         document: dict[str, dict[str, str]] = response.json()
         code: Optional[str] = extract_code(response=document)
         if code is not None:
