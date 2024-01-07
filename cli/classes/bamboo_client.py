00f0103e70edf4e73717134de3c06d5144a148bd
Andreas Resch <andreas@resch.io>
2024-01-07 18:01:42 +0100

    linting

--- cli/classes/bamboo_client.py
+++ cli/classes/bamboo_client.py
@@ -7,7 +7,7 @@ from typing import Optional, Tuple, Any, List
 
 import requests
 import yaml
-from starlette.exceptions import HTTPException  # type: ignore
+from fastapi import HTTPException
 
 from classes.bamboo_specs import (
     BambooSpecs,
